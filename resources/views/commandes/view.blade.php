53608278f6ab960be544a844bf4ce286578a9945
Salhi Ali <salhiali197@gmail.com>
2022-10-10 15:27:51 +0100

    swal

--- resources/views/commandes/view.blade.php
+++ resources/views/commandes/view.blade.php
@@ -17,10 +17,7 @@
 <div class="container-fluid">
     <h1 class="mt-4 text-white">Gestion Commandes</h1>
         <div class="card mb-4">
-        <div class="card-header">
-                <a class="btn btn-primary" href="{{route('commande.valider',['num_commande'=>$num_commande])}}">  Valider</a>
-                &nbsp;
-                <a class="btn btn-danger" href="{{route('commande.rejeter',['num_commande'=>$num_commande])}}">  rejeter</a>
+            <div class="card-header">
             </div>
 
             <div class="card-body">
@@ -32,6 +29,8 @@
                             <td align="left">
                             <h3>Nom Prénom : {{ App\Commande::get_client($num_commande)[0]->nom ?? '' }} {{ App\Commande::get_client($num_commande)[0]->prenom ?? '' }}</h3>
                             <h3> Téléphone : {{ App\Commande::get_client($num_commande)[0]->telephone ?? 'ss' }}</h3>
+                            <h3> Téléphone : {{ App\Commande::get_client($num_commande)[0]->wilaya ?? ''}}</h3>
+                            <h3> Téléphone : {{ App\Commande::get_client($num_commande)[0]->commune ?? '' }}</h3>
                             
                             </td>
                         </tr>
@@ -80,6 +79,23 @@
 
             </div>
 
+
+            <div class="card-footer text-center">
+                <div class="row">
+                    <div class="col-md-6">
+                        <button class="btn btn-primary" href="{{route('commande.valider',['num_commande'=>$num_commande])}}" id="btn_valider">  Valider</button>
+                    </div>
+
+                    <div class="col-md-6">
+                        <a class="btn btn-danger" href="{{route('commande.rejeter',['num_commande'=>$num_commande])}}">  rejeter</a>                
+                    </div>
+
+
+                </div>
+                    &nbsp;
+            </div>
+
+
         </div>
 
     </div>
@@ -95,4 +111,39 @@
 
         //
     </script>
+@endsection
+
+@section('scripts')
+<script>
+$( document ).ready(function() {
+		$("#btn_valider").on("click", function(e) {
+		swal({
+			title: "Etes Vous Sure ?",
+			text: "Confirmer La Validation ",
+			type: "warning",
+			showCancelButton: true,
+			confirmButtonColor: '#DD6B55',
+			confirmButtonText: 'Oui',
+			cancelButtonText: "Annuler",
+			closeOnConfirm: false,
+			closeOnCancel: false		
+		},
+		function(isConfirm){
+			if (isConfirm){
+				console.log(document.getElementById('transfer_form'))
+				// document.getElementById('transfer_form').submit();
+                window.location.href = '{{route('commande.valider',['num_commande'=>$num_commande])}}'; //Will take you to Google.
+				swal.close()
+					
+			 } else {
+			   swal("Annulé", "Vous avez annuler la validation", "error");
+				  e.preventDefault();
+			 }
+		});		 
+	});
+})
+
+
+
+</script>
 @endsection
